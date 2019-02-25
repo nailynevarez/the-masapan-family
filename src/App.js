e9595ee5fb7cb1f7690ab55072cbcfb7b9f9af7c
nailynevarez <nailyn@uoregon.edu>
2019-02-24 22:52:54 -0800

    Add hand image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import hand from './Menu-Hand.gif';
 import Menu from './Menu.js';
 import First from './First.js';
 import Second from './Second.js';
@@ -37,7 +37,18 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-
+          <img src={hand} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
         </header>
       </div>
     );
